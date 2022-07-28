813998f41b08fe09c7914bce4306d2e698a3ea44
yasinitsky <yasinitskiy@gmail.com>
2022-07-28 04:44:56 +0200

    add: amd64 types and functions

---
+++ include/fhook/arch/amd64/amd64.hpp
@@ -0,0 +1,51 @@
+/**
+ * This file is part of the fhook library.
+ * 
+ * SPDX-License-Identifier: 0BSD
+ * 
+ * Copyright (C) 2022
+ * Author: Yaroslav Yasynytskyi <yaroslav.yasynytskyi@gmail.com>
+*/
+
+/**
+ * This file contains all architecture-dependent types and functions for AMD64 (x86_64).
+*/
+
+#if defined __amd64__ || defined _M_AMD64
+#define FHOOK_ARCH_INCLUDED
+
+#ifndef FHOOK_ARCH_AMD64_HPP
+#define FHOOK_ARCH_AMD64_HPP
+
+#if defined _MSC_VER
+    typedef unsigned __int8 uint8_t;
+    typedef unsigned __int16 uint32_t;
+    typedef unsigned __int64 uint64_t;
+#else
+    #include <stdint.h>
+#endif
+
+namespace fhook
+{
+    #pragma pack(push, 1)
+    typedef struct
+    {
+        uint8_t push = 0x50;        // push rax
+        uint16_t mov = 0xb848;      // mov rax, ...
+        uint64_t address = 0x00;    // target address
+        uint16_t jmp = 0xe0ff;      // jmp rax
+    } Trampoline;
+    #pragma pack(pop)
+
+    Trampoline makeTrampoline(VoidPointer address)
+    {
+        Trampoline bytecode;
+        bytecode.address = (uint64_t) address;
+        return bytecode;
+    }
+
+}
+
+#endif // FHOOK_ARCH_AMD64_HPP
+
+#endif // CPU-specific macros
