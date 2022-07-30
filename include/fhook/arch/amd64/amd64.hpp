0c2d1bab510fe425d30b9a0914f5dbac8cf98d8d
yasinitsky <yasinitskiy@gmail.com>
2022-07-30 22:12:12 +0200

    refactoring: remove function implementation

--- include/fhook/arch/amd64/amd64.hpp
+++ include/fhook/arch/amd64/amd64.hpp
@@ -17,28 +17,25 @@
 #ifndef FHOOK_ARCH_AMD64_HPP
 #define FHOOK_ARCH_AMD64_HPP
 
-#if defined _MSC_VER
-    typedef unsigned __int8 uint8_t;
-    typedef unsigned __int32 uint32_t;
-    typedef unsigned __int64 uint64_t;
-    typedef __int64 int64_t;
-#else
-    #include <stdint.h>
-#endif
-
 namespace fhook
 {
+    #if defined _MSC_VER
+        typedef unsigned __int8 uint8_t;
+        typedef unsigned __int32 uint32_t;
+        typedef unsigned __int64 uint64_t;
+        typedef __int64 int64_t;
+    #else
+        #include <stdint.h>
+    #endif
+
     typedef int64_t LongestInteger;
 
     #pragma pack(push, 1)
     typedef struct
     {
         uint8_t push = 0x68;                // push
-        uint32_t lowerBytes = 0x00000000; 
-        uint8_t movOpcode = 0xc7;
-        uint8_t movModRM = 0x44;
-        uint8_t movSIB = 0x24;
-        uint8_t movOffset = 0x04;           // mov [rsp+4]
+        uint32_t lowerBytes = 0x00000000;
+        uint32_t mov = 0x042444c7;          // mov [rsp+4]
         uint32_t upperBytes = 0x00000000;
         uint8_t ret = 0xc3;                 // ret
     } Jump;
@@ -51,33 +48,17 @@ namespace fhook
         Jump jmp;
     } Trampoline;
     #pragma pack(pop)
-
-    Jump makeJump(VoidPointer address)
-    {
-        Jump bytecode;
-
-        bytecode.lowerBytes = ((uint32_t*) &address)[0];
-        bytecode.upperBytes = ((uint32_t*) &address)[1];
-
-        return bytecode;
-    }
-
-    Trampoline makeTrampoline(VoidPointer address)
-    {
-        Trampoline bytecode;
-
-        for(int i = 0; i < sizeof(bytecode.oldCode)/sizeof(bytecode.oldCode[0]); i++)
-        {
-            bytecode.oldCode[i] = 0x90; // nop
-        }
-
-        bytecode.jmp = makeJump(address);
-
-        return bytecode;
-    }
-
 }
 
 #endif // FHOOK_ARCH_AMD64_HPP
 
 #endif // CPU-specific macros
+
+/*
+
+uint8_t movOpcode = 0xc7;
+uint8_t movModRM = 0x44;
+uint8_t movSIB = 0x24;
+uint8_t movOffset = 0x04;
+
+*/
