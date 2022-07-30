44df24f0a934c20a7c1e6810a682f81084a8bae3
yasinitsky <yasinitskiy@gmail.com>
2022-07-30 21:58:14 +0200

    remove: library assembly logic

--- include/fhook/fhook.hpp
+++ include/fhook/fhook.hpp
@@ -8,48 +8,12 @@
 */
 
 /**
- * This file includes all header files in the correct order. The main header file of the library.
+ * This file is needed for compatibility with previous versions.
 */
 
 #ifndef FHOOK_HPP
 #define FHOOK_HPP
 
-#if defined _MSC_VER
-    typedef unsigned __int8 uint8_t;
-#else
-    #include <stdint.h>
-#endif
-
-namespace fhook
-{
-    typedef void* VoidPointer;
-    typedef uint8_t* BytePointer;
-}
-
-/**
- * OS-specific headers.
- * To add support for a new operating system, just add its header file here
- * (provided that the OS header file is written according to the documentation).
-*/
-#include "os/unix/unix.hpp"
-
-#if !defined FHOOK_OS_INCLUDED
-    #error "fhook: unsupported operating system"
-#endif
-
-
-/**
- * CPU-specific headers.
- * To add support for a new architecture, just add its header file here
- * (provided that the architecture header file is written according to the documentation).
-*/
-#include "arch/amd64/amd64.hpp"
-
-#if !defined FHOOK_ARCH_INCLUDED
-    #error "fhook: unsupported architecture"
-#endif
-
-#include "exceptions.hpp"
-#include "general.hpp"
+#include "hook.hpp"
 
 #endif // FHOOK_HPP
